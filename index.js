3f687ce2d0d41360d99b97a575a3ee0e5228d6e1
Altenia Gaines <gaines.tina90@gmail.com>
2023-03-08 15:33:15 -0700

    create objects

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jane",
+    streetAddress: "123 Main St.",
+};
+
+function updateEmployeeWithKeyAndValue(empObj,key,value){
+    const newObj = {...empObj};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(empObj,key,value){
+    empObj[key] = value;
+    return empObj;
+}
+
+function deleteFromEmployeeByKey(empObj,key,){
+    const delObj = {...empObj};
+    delete delObj[key];
+    return delObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(empObj,key){
+    delete employee[key];
+    return empObj;
+}
